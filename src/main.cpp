7e2dff84772e67a4771bc6b2a1dc05701273e500
Victor <vic.zkj@gmail.com>
2021-10-14 23:47:33 +0800

    重写命令处理2

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@
  * Author: Chia Sang
  *
  */
-#include <stdlib.h>
+#include <Ticker.h>
 #include <Arduino.h>
 #include <ArduinoLog.h>
 #include <CmdParser.hpp>
@@ -14,28 +14,26 @@
 #define TOUCHPIN0 4
 #define TOUCH_THRESHOLD 30
 
+Ticker tickerGetDown;
+Ticker tickerReportTemp;
+
 CmdParser cmdParser;
 
-QueueHandle_t queueGetting;
-QueueHandle_t queueSetting;
-
-TaskHandle_t uart2_receive_handler = NULL;
-TaskHandle_t execute_cmd_handler = NULL;
-TaskHandle_t report_state_handler = NULL;
-TaskHandle_t parserCMD_handler = NULL;
-TaskHandle_t uart2_send_handler = NULL;
-
 boolean deviceState = 0;
-boolean loopState = true;
 
-int queueSize = 512;
 int runState = 1;
 int deviceMode = 2;
-int heatTemperature = 80;
-int keepTemperature = 30;
-
 int currentTemperature = 26;
 int settingTemperature = 95;
+int ind1;
+int ind2;
+int ind3;
+int ind4;
+
+String angle;
+String fuel;
+String speed1;
+String altidude;
 
 // void InitialDevice();
 // void printLogLevel();
@@ -118,78 +116,86 @@ void device_switch()
 void ticktick()
 {
   runState = !runState;
-  Serial.printf("deviceState: %d\n", runState);
+  Log.notice("deviceState: %d" CR, runState);
 }
 
 // Report parameter state task
-void report_state(void *parameters)
+void report_state()
 {
   for (;;)
   {
     // settingTemperature = random(25, 101);
     currentTemperature = random(25, 101);
-    // String temperature = String(random(50, 100));
-    // String msg = "properties_changed 2 5 " + temperature + "\r";
-    // char buffer[msg.length()];
-    // msg.toCharArray(buffer, msg.length() + 1);
-    // Serial2.write(buffer);
-    // loopState = true;
+    String temperature = String(random(50, 100));
+    String msg = "properties_changed 2 5 " + temperature + "\r";
+    char buffer[msg.length()];
+    msg.toCharArray(buffer, msg.length() + 1);
+    Serial2.write(buffer);
+
     vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
 }
 
+String getValue(String data, char separator, int index)
+{
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++)
+  {
+    if (data.charAt(i) == separator || i == maxIndex)
+    {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
+
+void parseCMD(String readString)
+{
+  ind1 = readString.indexOf(' ');                  //finds location of first ,
+  angle = readString.substring(0, ind1);           //captures first data String
+  ind2 = readString.indexOf(' ', ind1 + 1);        //finds location of second ,
+  fuel = readString.substring(ind1 + 1, ind2 + 1); //captures second data String
+  ind3 = readString.indexOf(' ', ind2 + 1);
+  speed1 = readString.substring(ind2 + 1, ind3 + 1);
+  ind4 = readString.indexOf(' ', ind3 + 1);
+  altidude = readString.substring(ind3 + 1);
+
+  Log.notice("命令0: %s" CR, angle);
+  Log.notice("命令1: %s" CR, fuel);
+  Log.notice("命令2: %d" CR, speed1);
+  Log.notice("命令3: %d" CR, altidude);
+}
+
 // ======================================================================
 // Parse command and assign tasks corresponding to commands
-void parseCMD(const char *cmd)
+void executeCMD(const char *cmd)
 {
-  int len = strlen(cmd);
-  char *newStr = new char[len + 1];
-  for (int k = 0; k < len; k++)
+  if (cmdParser.parseCmd((char *)cmd) != CMDPARSER_ERROR)
   {
-    newStr[k] = cmd[k];
-  }
-  newStr[len] = '\0';
+    // const size_t count = cmdParser.getParamCount();
+    const char *siid = cmdParser.getCmdParam(1);
+    const char *piid = cmdParser.getCmdParam(2);
 
-  Log.notice("接收到的命令为: %s" CR, cmd, strlen(cmd));
-  Log.notice("长度为: %d" CR, strlen(cmd));
+    Log.notice(F("Command: %s Param1: %s Param2: %s" CR), cmdParser.getCommand(), siid, piid);
 
-  // for (size_t i = 0; i < strlen(cmd); i++)
-  // {
-  //   if (cmd[i] != ' ')
-  //   {
-  //     /* code */
-  //   }
-  // }
-  // char ec[] = "down get_properties 2 1\r";
-
-  // if (strcmp(ec, cmd) == 0)
-  // {
-  //   Serial2.write("result 2 1 0 0\r");
-  //   Log.notice("应答" CR);
-  // }
-
-  if (cmdParser.parseCmd(newStr) != CMDPARSER_ERROR)
-  {
-    //   // const size_t count = cmdParser.getParamCount();
-    const char *siid = cmdParser.getCmdParam(2);
-    const char *piid = cmdParser.getCmdParam(3);
-
-    // get_properties
     if (cmdParser.equalCommand("get_properties"))
     {
-      Log.notice("解析的命令为: %s" CR, cmdParser.getCommand());
       if (*siid == '2')
       {
         switch (*piid)
         {
+
         case '1':
-        { // attribute 1
-          // Serial.printf("receive_%s parser get 1\n", cmd.c_str());
-          // char const *echo_cmd = "result 2 1 0 0\r";
-          // loopState = true;
+        {
           Serial2.write("result 2 1 0 0\r");
           break;
         }
+
         case '2':
         {
           if (runState)
@@ -200,48 +206,55 @@ void parseCMD(const char *cmd)
           {
             Serial2.write("result 2 2 0 0\r");
           }
-          loopState = true;
           break;
         }
+
         case '3':
         {
 
-          char msg[] = "result 2 3 0 ";
-          char c[2];
-          itoa(runState, c, 10);
-          // Serial.println(strcat(msg, strcat(c, "\r")));
-          Serial2.write(strcat(msg, strcat(c, "\r")));
-          loopState = true;
+          // char msg[] = "result 2 3 0 ";
+          // char c[2];
+          // itoa(runState, c, 10);
+          // // Serial.println(strcat(msg, strcat(c, "\r")));
+          // Serial2.write(strcat(msg, strcat(c, "\r")));
+
+          Serial2.write("result 2 3 0 1\r");
           break;
         }
+
         case '4':
         {
 
-          char msg[] = "result 2 4 0 ";
-          char c[2];
-          itoa(deviceMode, c, 10);
-          Serial2.write(strcat(msg, strcat(c, "\r")));
-          loopState = true;
+          // char msg[] = "result 2 4 0 ";
+          // char c[2];
+          // itoa(deviceMode, c, 10);
+          // Serial2.write(strcat(msg, strcat(c, "\r")));
+
+          Serial2.write("result 2 4 0 1\r");
           break;
         }
+
         case '5':
         {
 
-          char msg[] = "result 2 5 0 ";
-          char c[4];
-          itoa(currentTemperature, c, 10);
-          Serial2.write(strcat(msg, strcat(c, "\r")));
-          loopState = true;
+          // char msg[] = "result 2 5 0 ";
+          // char c[4];
+          // itoa(currentTemperature, c, 10);
+          // Serial2.write(strcat(msg, strcat(c, "\r")));
+
+          Serial2.write("result 2 5 0 45\r");
           break;
         }
+
         case '6':
         {
 
-          char msg[] = "result 2 6 0 ";
-          char c[4];
-          itoa(settingTemperature, c, 10);
-          Serial2.write(strcat(msg, strcat(c, "\r")));
-          loopState = true;
+          // char msg[] = "result 2 6 0 ";
+          // char c[4];
+          // itoa(settingTemperature, c, 10);
+          // Serial2.write(strcat(msg, strcat(c, "\r")));
+
+          Serial2.write("result 2 6 0 68\r");
           break;
         }
         default:
@@ -250,12 +263,9 @@ void parseCMD(const char *cmd)
       }
     }
 
-    // set_properties
     else if (cmdParser.equalCommand("set_properties"))
     {
-      const char *siid = cmdParser.getCmdParam(2);
-      const char *piid = cmdParser.getCmdParam(3);
-      const char *val = cmdParser.getCmdParam(4);
+      const char *val = cmdParser.getCmdParam(3);
       if (*siid == '2')
       {
         switch (*piid)
@@ -264,22 +274,27 @@ void parseCMD(const char *cmd)
         case '2':
         {
           runState = *val - '0';
-          Serial.printf("设备状态: %d", runState);
+          Log.notice("设备状态: %d" CR, runState);
           Serial2.write("result 2 2 0\r");
           Serial2.write("properties_changed 2 2 1\r");
-          loopState = true;
           break;
         }
 
         case '4':
         {
-          Serial.printf("parser set 4\n");
+          deviceMode = *val - '0';
+          Log.notice("设备模式: %d" CR, deviceMode);
+          Serial2.write("result 2 4 0\r");
+          Serial2.write("properties_changed 2 2 2\r");
           break;
         }
 
         case '6':
         {
-          Serial.printf("parser set 6\n");
+          settingTemperature = atoi(val);
+          Log.notice("设定温度: %d" CR, settingTemperature);
+          Serial2.write("result 2 6 0\r");
+          Serial2.write("properties_changed 2 6 49\r");
           break;
         }
 
@@ -291,7 +306,7 @@ void parseCMD(const char *cmd)
 
     else
     {
-      Log.notice("Command: %s No Action" CR, cmd);
+      Log.verbose("Command: %s No Action" CR, cmd);
     }
   }
 }
@@ -301,7 +316,6 @@ void parseCMD(const char *cmd)
 void getDown()
 {
   String inputString = "";
-  // char *cmd;
   if (Serial2.available())
   {
     while (Serial2.available())
@@ -313,8 +327,18 @@ void getDown()
       }
     }
     // cmd = &inputString[0];
-    parseCMD(inputString.c_str());
-    Log.notice("recv: %s" CR, inputString.c_str());
+    inputString.trim();
+    String gcmd = inputString.substring(5);
+    if (gcmd != "none")
+    {
+      // parseCMD(gcmd);
+      executeCMD(inputString.substring(5).c_str());
+      Log.verbose("recv_p: %s" CR, inputString.substring(5).c_str());
+    }
+    else
+    {
+      Log.verbose("recv: %s" CR, inputString.c_str());
+    }
     // char buffer[inputString.length()];
     // inputString.toCharArray(buffer, inputString.length());
   }
@@ -394,15 +418,12 @@ void InitialDevice()
 void setup()
 {
   InitialDevice();
-  // xTaskCreatePinnedToCore(uart2_send, "uart2_send", 1024, NULL, 1, &uart2_send_handler, 1);
-  // xTaskCreatePinnedToCore(uart2_receive, "uart2_receive", 1024, NULL, 1, &uart2_receive_handler, 1);
-  // xTaskCreatePinnedToCore(report_state, "report_state", 4096, NULL, 1, &report_state_handler, 1);
-  // xTaskCreatePinnedToCore(execute_cmd, "execute_cmd", 4096, NULL, 1, &execute_cmd_handler, 0);
-  // xTaskCreatePinnedToCore(parseCMD, "parseCMD", 2048, NULL, 1, &parserCMD_handler, 1);
+  tickerGetDown.attach_ms(50, getDown);
+  tickerReportTemp.attach_ms(5000, report_state);
 }
 
 void loop()
 {
-  getDown();
-  delay(60);
+  // getDown();
+  // delay(100);
 }
